4e8a3cd8cc9db4078e0d278cb617568c23e819ec
unknown <169750@BME.HU>
2024-10-09 09:22:49 +0200

    Random value fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -7,7 +7,9 @@ import java.util.Random;
 *
 * (Deliberately contains bugs.)
 */
-public class TorpedoStore {
+public class TorpedoStore {  
+  
+  private Random random = new Random();
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
@@ -35,9 +37,8 @@ public class TorpedoStore {
 
     boolean success = false;
 
-    // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    // simulate random overheating of the launcher bay which prevents firing 
+    double r = this.random.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
